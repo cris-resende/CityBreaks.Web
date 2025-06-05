f34255e71c8a58e0f3ee36952016ed41108c47d0
Cristyan Resende <cristyan.souza@al.infnet.edu.br>
2025-06-05 20:09:09 -0300

    Feature: Exercicio 2 - Criando a Entidade Country e Aplicando a Primeira Migração

---
+++ Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityBreaks.Web.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string CountryName { get; set; }
+    }
+}
